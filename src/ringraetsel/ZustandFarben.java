4a3dd54a840aa7928c843b34846e251ff0d2706e
GOga <gogannes@googlemail.com>
2013-12-26 14:58:36 +0100

    farbenreihen

--- src/ringraetsel/ZustandFarben.java
+++ src/ringraetsel/ZustandFarben.java
@@ -2,59 +2,83 @@ package ringraetsel;
 
 import java.util.ArrayList;
 import java.util.List;
-import static ringraetsel.Farbe.*;
 
-public class ZustandFarben extends AbstracZustand<Farbe>{
+import ringraetsel.ZustandFarben.Farbe;
 
-    List<Farbe> rechts = new ArrayList<Farbe>(20);
-    List<Farbe> links = new ArrayList<Farbe>(20);
+public class ZustandFarben extends AbstracZustand<Farbe> {
 
-    /*
-     * 1. Schnittpunkt: rechts[0]=links[0]
-     * 
-     * 2. Schnittpunkt: rechts[15]=links[15]
-     * 
-     * ROT:10 BLAU:10 SCHWARZ:9 GELB:9
-     */
-
-    public void reset() {
-	for (int i = 1; i < 11; i++) {
-	    rechts.set(i, Farbe.R);
-	}
-	
-	for (int i = 11; i < 20; i++) {
-	    rechts.set(i, Farbe.S);
-	}
-	links.set(15, Farbe.S);
-
-	for (int i = 16; i < 26; i++) {
-	    links.set(i % 20, Farbe.B);
-	}
-	rechts.set(0, Farbe.B);
-
-	for (int i = 26; i < 26 + 9; i++) {
-	    links.set(i % 20, Farbe.G);
-	}
-    }
-
-    public ZustandFarben() {
-	for (int i = 0; i < 20; i++) {
-	    rechts.add(Farbe.B);
-	    links.add(Farbe.B);
+	public enum Farbe {
+		R, B, S, G;
 	}
 
-	reset();
-    }
+	List<Farbe> rechts = new ArrayList<Farbe>(20);
+	List<Farbe> links = new ArrayList<Farbe>(20);
 
-    @Override
-    public String toString() {
-	String result = "";
+	/*
+	 * 1. Schnittpunkt: rechts[0]=links[0]
+	 * 
+	 * 2. Schnittpunkt: rechts[15]=links[15]
+	 * 
+	 * ROT:10 BLAU:10 SCHWARZ:9 GELB:9 tatsächlich ROT:10 Schwarz:10 blau:9
+	 * GELB:9
+	 */
 
-	result += "li:\n" + links;
-	result += "\n";
-	result += "re:\n" + rechts;
+	public void problem() {
+		Farbe[] farbenreihe_rechts = new Farbe[] { R, G };
+		Farbe[] farbenreihe_links = new Farbe[] { R, G };
+		for (int i = 0; i < 20; i++) {
+			rechts.set(i, farbenreihe_rechts[i]);
+		}
+		for (int i = 0; i < 20; i++) {
+			links.set(i, farbenreihe_links[i]);
+		}
+	}
 
-	return result;
-    }
+	public void reset() {
+		// rechter kreis
+		for (int i = 0; i < 5; i++) {
+			rechts.set(i, Farbe.G);
+		}
+		for (int i = 5; i < 15; i++) {
+			rechts.set(i, Farbe.S);
+		}
+		for (int i = 15; i < 16; i++) {
+			rechts.set(i, Farbe.R);
+		}
+		for (int i = 16; i < 20; i++) {
+			rechts.set(i, Farbe.G);
+		}
+		// linker kreis
+		for (int i = 0; i < 1; i++) {
+			links.set(i, Farbe.G);
+		}
+		for (int i = 1; i < 10; i++) {
+			links.set(i, Farbe.B);
+		}
+		for (int i = 10; i < 20; i++) {
+			links.set(i, Farbe.R);
+		}
+
+	}
+
+	public ZustandFarben() {
+		for (int i = 0; i < 20; i++) {
+			rechts.add(Farbe.B);
+			links.add(Farbe.B);
+		}
+
+		reset();
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+
+		result += "li:\n" + links;
+		result += "\n";
+		result += "re:\n" + rechts;
+
+		return result;
+	}
 
 }
